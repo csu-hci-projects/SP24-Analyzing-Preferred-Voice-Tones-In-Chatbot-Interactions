1085d40341e313ba6eab74cbee8448f7975ce08d
kitten <kitten@colostate.edu>
2024-04-12 18:45:05 -0700

    AddOptionsCode

--- RecordToFile.java
+++ RecordToFile.java
@@ -9,11 +9,14 @@ import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileWriter;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RecordToFile {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8999), 0);
         server.createContext("/rating", new RatingHandler());
+        // server.createContext("/test", new RatingHandler());
         server.start();
     }
     
@@ -52,4 +55,45 @@ public class RecordToFile {
             }
         }
     }
+
+    
+    // static class FileHandler implements HttpHandler {
+    //     @Override
+    //     public void handle(HttpExchange exchange) throws IOException {
+    //         if ("GET".equals(exchange.getRequestMethod())) {
+    //             String query = exchange.getRequestURI().getQuery();
+    //             String[] queryParams = query.split("=");
+    //             if (queryParams.length == 2 && queryParams[0].equals("filename")) {
+    //                 String filename = queryParams[1];
+    //                 try {
+    //                     // Read the content of the file
+    //                     String content = new String(Files.readAllBytes(Paths.get(filename)));
+    //                     exchange.sendResponseHeaders(200, content.getBytes().length);
+    //                     OutputStream os = exchange.getResponseBody();
+    //                     os.write(content.getBytes());
+    //                     os.close();
+    //                 } catch (IOException e) {
+    //                     e.printStackTrace();
+    //                     String response = "File not found or error reading file.";
+    //                     exchange.sendResponseHeaders(404, response.getBytes().length);
+    //                     OutputStream os = exchange.getResponseBody();
+    //                     os.write(response.getBytes());
+    //                     os.close();
+    //                 }
+    //             } else {
+    //                 String response = "Invalid request";
+    //                 exchange.sendResponseHeaders(400, response.getBytes().length);
+    //                 OutputStream os = exchange.getResponseBody();
+    //                 os.write(response.getBytes());
+    //                 os.close();
+    //             }
+    //         } else {
+    //             String response = "Not supported";
+    //             exchange.sendResponseHeaders(405, response.getBytes().length);
+    //             OutputStream os = exchange.getResponseBody();
+    //             os.write(response.getBytes());
+    //             os.close();
+    //         }
+    //     }
+    // }
 }
